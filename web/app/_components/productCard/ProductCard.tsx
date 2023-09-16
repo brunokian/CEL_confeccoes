6d6db1cc03d8a302fdc33bb381736462a1bf57da
brunokian <brunokian@hotmail.com>
2023-09-16 17:55:13 -0300

    testando hover

--- web/app/_components/productCard/ProductCard.tsx
+++ web/app/_components/productCard/ProductCard.tsx
@@ -1,19 +1,54 @@
+"use client"
+
 import Image from "next/image"
+import { useState } from "react";
 import { fraudas_images } from "@/assets/fraudas_images"
 
-export default function ProductCard({item}: any) {
-    return (
-        <div className="flex flex-col items-center">
-            <div className="bg-white p-2 rounded-t-3xl text-center text-lg max-w-[300px] w-full">
-                <h1>{item.name}</h1>
-            </div>
-            <Image
-                src={item.image}
-                alt=""
-                width={300}
-                height={300}
-                className="rounded-b-3xl shadow-xl"
-            />
+
+export default function ProductCard({ item }: any) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div className="relative">
+      <div
+        className="flex flex-col items-center cursor-pointer"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Image
+          src={item.image}
+          alt=""
+          width={300}
+          height={300}
+          className="rounded-3xl shadow-xl"
+        />
+      </div>
+
+      {isHovered && (
+        <div
+          className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50"
+          style={{ width: '300px', height: '300px' }}
+        >
+          {/* Componente de sobreposição com informações do produto */}
+          <div className="bg-white p-4 rounded-lg shadow-lg">
+            {/* Conteúdo das informações do produto */}
+            {/* Substitua isso pelo conteúdo real das informações do produto */}
+            <h2 className="text-xl font-semibold">{item.name}</h2>
+            <p className="text-gray-700">{item.description}</p>
+
+            {/* Botão para fechar o modal */}
+           
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./_components/productCard/ProductCard"
 
 export default function Home() {
   return (
-     <div className="min-h-[1000px] h-full max-w-[1200px] m-auto bg-orange-50 flex justify-center pt-10">
+     <div className="min-h-[1000px] h-full max-w-[1200px] m-auto bg-gradient-to-r from-white to-orange-100 flex justify-center pt-10">
    
 
         <div className="grid md:grid-cols-3 grid-cols-2 gap-8 p-4 w-[1000px]">
